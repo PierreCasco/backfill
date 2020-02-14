f4df0d41734189798ccbc4766f1a11cdfc326b2a
Pierre Casco <48262269+PierreCasco@users.noreply.github.com>
2020-02-14 12:16:41 -0500

    Add files via upload

---
+++ backfill_run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 15:45:14 2020
+
+@author: pierre.casco
+"""
+
+#import cumulus_matching
+import combining_files
+
+# File 1 is cumulus file
+# File 2 is backfill file
+# path with filename
+
+#cumulus_matching.cumulus_run('Cumulus 2018 Data.xlsx','backfill.csv',
+#                             '/Users/pierre.casco/backfill/Files/test.csv')
+
+combining_files.run_knack()
